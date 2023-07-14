524d2c47fd774b9d0a25e7513f8c01453b909164
78nick <138786569+78nick@users.noreply.github.com>
2023-07-14 14:09:16 +0300

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,13 +1,13 @@
 const cards = [
-   "img/1.jpg",
-   "img/2.jpg",
-   "img/3.jpg",
-   "img/4.jpg",
-   "img/5.jpg",
-   "img/6.jpg",
-   "img/7.jpg",
-   "img/8.jpg",
-   "img/Рубашка.jpg",
+   "../img/1.jpg",
+   "../img/2.jpg",
+   "../img/3.jpg",
+   "../img/4.jpg",
+   "../img/5.jpg",
+   "../img/6.jpg",
+   "../img/7.jpg",
+   "../img/8.jpg",
+   "../img/Рубашка.jpg",
 ];
 
 export const back = [
